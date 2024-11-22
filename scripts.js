e829ca35441105845a989f563617fa43dc5e6153
Nguyen Viet <nguyenviet02.dev@gmail.com>
2024-11-22 13:24:24 +0700

    a

--- scripts.js
+++ scripts.js
@@ -3,9 +3,23 @@ const closeBtn = document.getElementById('close-btn');
 const sidebar = document.getElementById('sidebar');
 
 menuBtn.addEventListener('click', () => {
-  sidebar.classList.add('show');
+	sidebar.classList.add('show');
 });
 
 closeBtn.addEventListener('click', () => {
-  sidebar.classList.remove('show');
+	sidebar.classList.remove('show');
+});
+
+const btnCA = document.getElementById('btnCA');
+
+const copyCA = () => {
+	window.navigator.clipboard.writeText("BzPcC3X5fDNiCAisNHau54DZJheYbv5QgEmqXYcEpump")
+}
+
+btnCA.addEventListener('click', () => {
+	copyCA();
+	btnCA.innerHTML = "Copied!";
+	setTimeout(() => {
+		btnCA.innerHTML = "Copy";
+	}, 1000);
 });
